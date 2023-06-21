d7370fc49fb50626477f5f9aee2c34ae8c5ee2e6
takahara tomoya <t.tkorock221@gmail.com>
2023-06-21 10:05:52 +0900

    reactiveに変更した

--- vitest-project/components/Form.vue
+++ vitest-project/components/Form.vue
@@ -1,23 +1,47 @@
 <script setup lang="ts">
-    const value = ref<string>('');
-    const errorFlag = ref<boolean>(false);
-    const valueUpdate = (event: Event) => {
-        if(!(event.target instanceof HTMLInputElement)){
-            return
-        }
-        value.value = event.target.value;
+    const userInformation = reactive({
+        tel: '',
+        mail: ''
+    });
 
-        errorFlag.value = !value.value.includes('@') ? true : false;
+    const errorFlag = reactive({
+        tel: false,
+        mail: false
+    })
+
+    type UserInformationKey = keyof (typeof userInformation)
+    const valueUpdate = (event: Event, id: UserInformationKey) => {
+        if(!(event.target instanceof HTMLInputElement)){ return }
+        userInformation[id] = event.target.value;
+
+        switch(id){
+            case 'mail':
+                errorFlag[id] = !userInformation[id].includes('@') ? true : false;
+                break;
+            case 'tel':
+                errorFlag[id] = isNaN(Number(userInformation[id])) ? true : false;
+        }
     }
 </script>
 
 <template>
-    <fieldset>
-        <legend>テストフォーム</legend>
-        <label>
-            メールアドレス
-            <input type="text" placeholder="めーるあどれす" @blur="valueUpdate" :value="value">
-            <div v-if="errorFlag">入力文字に"@"を入れてください。</div>
-        </label>
-    </fieldset>
+    <form action="" method="get" class="form-example">
+        <fieldset>
+            <legend>個人情報</legend>
+            <div>
+                <label>
+                    メールアドレス
+                    <input type="text" placeholder="めーるあどれす" @blur="valueUpdate($event,'mail')" :value="userInformation.mail">
+                    <div v-if="errorFlag.mail">入力文字に"@"を入れてください。</div>
+                </label>
+            </div>
+            <div>
+                <label>
+                    電話番号
+                    <input type="tel" placeholder="電話番号" @blur="valueUpdate($event,'tel')" :value="userInformation.tel">
+                    <div v-if="errorFlag.tel">数値で入力してください</div>
+                </label>
+            </div>
+        </fieldset>
+    </form>
 </template>
