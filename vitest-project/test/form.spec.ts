d9287cacff40e5d6b71932543774175ed47fd35c
takahara tomoya <t.tkorock221@gmail.com>
2023-06-23 09:50:39 +0900

    setupに切り出したものをテストに入れ込んだ

--- vitest-project/test/form.spec.ts
+++ vitest-project/test/form.spec.ts
@@ -1,32 +1,27 @@
 import { describe, expect, test } from 'vitest';
-import { render , screen} from '@testing-library/vue'
+import { screen} from '@testing-library/vue'
 import  userEvent from '@testing-library/user-event'
-import Form from '../components/Form.vue'
-
-const user = userEvent.setup();
+import {reRender, inputtedMail, inputtedTel, clickButton} from './setup/form.test'
 
 describe('FormComponent', () => {
     test('dummyValue Input', async() =>{
-        render(Form)
-        const form = screen.getByRole('textbox',{name: 'メールアドレス'});
-        screen.debug(form);
-        const mailValue = 'test@mail.com';
-        await user.type(form, mailValue);
-        expect(screen.getByDisplayValue(mailValue)).toBeInTheDocument();
+        reRender()
+        const value = 'test@mail.com'
+        await inputtedMail(value);
+        expect(screen.getByDisplayValue(value)).toBeInTheDocument();
     })
 
     test('first not display alert', () =>{
-        render(Form)
-        // ない可能性があるためクエリにした
+        reRender()
+        // NOTE: ない可能性があるためクエリにした
         const alert = screen.queryByText('入力文字に"@"を入れてください。');
         expect(alert).not.toBeInTheDocument();
     })
 
     test('dummyValue Inputted display alert', async() =>{
-        render(Form)
-        const form = screen.getByRole('textbox',{name: 'メールアドレス'});
-        const mailValue = 'test';
-        await user.type(form, mailValue);
+        reRender()
+        const value = 'test'
+        await inputtedMail(value);
         await userEvent.tab();
         expect(screen.getByText('入力文字に"@"を入れてください。')).toBeInTheDocument();
     })
--- vitest-project/test/setup/form.test.ts
+++ vitest-project/test/setup/form.test.ts
@@ -1,24 +1,25 @@
 import { render , screen} from '@testing-library/vue'
 import  userEvent from '@testing-library/user-event'
-import Form from '../components/Form.vue'
+import Form from '../../components/Form.vue'
 
 const user = userEvent.setup();
 
-export const setup = async() => {
+const reRender = () => {
     render(Form);
-    const inputtedMail = async(mail:string) => {
-        const mailForm = screen.getByRole('text-box', {name: 'メールアドレス'});
-        await user.type(mailForm, mail);
-    }
-
-    const inputtedTel = async(tel:string) => {
-        const telForm = screen.getByRole('text-box', {name: '電話番号'});
-        await user.type(telForm, tel);
-    }
-
-    const clickButton = async(name: string) => {
-        await user.click(screen.getByRole('button', {name}))
-    }
-
-    return {inputtedMail, inputtedTel, clickButton}
 }
+
+const inputtedMail = async(mail:string) => {
+    const mailForm = screen.getByRole('textbox', {name: 'メールアドレス'});
+    await user.type(mailForm, mail);
+}
+
+const inputtedTel = async(tel:string) => {
+    const telForm = screen.getByRole('textbox', {name: '電話番号'});
+    await user.type(telForm, tel);
+}
+
+const clickButton = async(name: string) => {
+    await user.click(screen.getByRole('button', {name}))
+}
+
+export {reRender, inputtedMail, inputtedTel, clickButton}
