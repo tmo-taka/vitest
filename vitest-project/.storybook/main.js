bf339890fee26766eba3f440daf51a517fa06bbc
takahara tomoya <t.tkorock221@gmail.com>
2023-06-24 10:44:18 +0900

    propsカスタマイズできるようにした

--- vitest-project/.storybook/main.js
+++ vitest-project/.storybook/main.js
@@ -5,6 +5,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-controls"
   ],
   framework: {
     name: "@storybook/vue3-vite",
--- vitest-project/components/stories/List.stories.ts
+++ vitest-project/components/stories/List.stories.ts
@@ -9,8 +9,28 @@ export default {
 }
 
 export const Template: Story  = {
-    render: () => ({
+    render: (args) => ({
         components: { List },
-        template: "<List />",
+        setup() {
+            return { args };
+        },
+        template: "<List :lists='args'/>",
     })
+};
+
+export const Test: Story = {
+    args: {
+        lists: [
+            {
+                title: 'すごい文字数のタイトル',
+                tags: ['1個目のタグ','2個目のタグ'],
+                text: 'ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト'
+            },
+            {
+                title: 'これもすごい文字数のタイトル',
+                tags: ['1個目のタグ','2個目のタグ','3個目のタグ'],
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        ]
+    },
 };
